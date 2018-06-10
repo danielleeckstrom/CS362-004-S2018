d304768913363dac1373b7faaa274592c9e47642
danielleeckstrom <eckstrod@oregonstate.edu>
2018-06-10 11:57:30 -0600

    Simplified port testing, switched validity statements to true/false

--- projects/eckstrod/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/eckstrod/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -27,20 +27,20 @@ public class UrlValidatorTest extends TestCase
 	   
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 	   
-	   System.out.println("Should be Invalid/False:\n");
-	   System.out.println(urlVal.isValid("3ht://256.256.256.256:-1/.."));
-	   System.out.println(urlVal.isValid("http:/.1.2.3.4/..//file?action=view"));
-	   System.out.println(urlVal.isValid("h3t://255.255.255.255:65a/$23"));
-	   System.out.println(urlVal.isValid("http/aaa:65a/test1/file?action=view"));
-	   System.out.println(urlVal.isValid("http:/.1.2.3.4:65636"));
+	   System.out.println("Should be Invalid/False:");
+	   System.out.println(urlVal.isValid("3ht://256.256.256.256:-1/..")); //correctly evaluated as false
+	   System.out.println(urlVal.isValid("http:/.1.2.3.4/..//file?action=view")); //correctly evaluated as false
+	   //System.out.println(urlVal.isValid("h3t://255.255.255.255:65a/$23")); //fails to return, crashes test
+	   System.out.println(urlVal.isValid("http/aaa:65a/test1/file?action=view")); //correctly evaluated as false
+	   System.out.println(urlVal.isValid("http:/.1.2.3.4:65636")); //incorrectly evaluated as true 
 
 	   
-	   System.out.println("Should be Valid/True:\n");
-	   System.out.println(urlVal.isValid("http://www.google.com:80/test1?action=view"));
-	   System.out.println(urlVal.isValid("ftp://go.com/test1/file?action=edit&mode=up"));
-	   System.out.println(urlVal.isValid("h3t://www.google.com:65535?action=view"));
-	   System.out.println(urlVal.isValid("ftp://www.google.com:0"));
-	   System.out.println(urlVal.isValid("h3t://0.0.0.0:80/test1?action=edit&mode=up"));
+	   System.out.println("Should be Valid/True:");
+	   System.out.println(urlVal.isValid("http://www.google.com:80/test1?action=view")); //correctly evaluated as true 
+	   System.out.println(urlVal.isValid("ftp://go.com/test1/file?action=edit&mode=up")); //fails to return, crashes test
+	   System.out.println(urlVal.isValid("h3t://www.google.com:65535?action=view")); //fails to return, crashes test
+	   System.out.println(urlVal.isValid("ftp://www.google.com:0")); //fails to return, crashes test
+	   System.out.println(urlVal.isValid("h3t://0.0.0.0:80/test1?action=edit&mode=up")); //fails to return, crashes test
 	   
    }
    
@@ -50,11 +50,10 @@ public class UrlValidatorTest extends TestCase
 	   //You can use this function to implement your First Partition testing
 	   String testName = "Partitioned - Scheme";
 
-	   System.out.println("Test: " + testName);
+	   System.out.println("\nTest: " + testName);
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 	   String[] testScheme = {"http://", "ftp://", "h3t://", "http:/", "http:", "http"};  
-	   String[] schemeValidity = {"Valid", "Valid", "Valid", "Invalid", "Invalid", "Invalid"}; 
-	   //String[] expectedURLValidity = {"?", "?", "?", "?", "?", "?"};
+	   boolean[] schemeValidity = {true, true, true, false, false, false}; 
 	   int i;
 	   
 	   for(i = 0; i < testScheme.length; i++)
@@ -74,11 +73,10 @@ public class UrlValidatorTest extends TestCase
 	   //You can use this function to implement your Second Partition testing	   
 	   String testName = "Partitioned - Authority";
 
-	   System.out.println("Test: " + testName);
+	   System.out.println("\nTest: " + testName);
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 	   String[] testAuthority= {"google.com", "oregonstate.edu", "wikipedia.org", "256.256.256.256", "http:", "http"};  
-	   String[] authorityValidity = {"Valid", "Valid", "Valid", "Invalid", "Invalid", "Invalid"}; 
-	   //String[] expectedURLValidity = {"?", "?", "?", "?", "?", "?"};
+	   boolean[] authorityValidity = {true, true, true, false, false, false}; 
 	   int i;
 	   
 	   for(i = 0; i < testAuthority.length; i++)
@@ -97,26 +95,22 @@ public class UrlValidatorTest extends TestCase
    {
 	   //You can use this function to implement your Second Partition testing	   
 	   String testName = "Partitioned - Port";
-	   System.out.println("Test: " + testName);
+	   System.out.println("\nTest: " + testName);
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   String[] testPort = {":80", ":0", "", ":-1", ":65363", ":65a"};  
-	   String[] portValidity = {"Valid", "Valid", "Valid", "Invalid", "Invalid", "Invalid"}; 
-	   //String[] expectedURLValidity = {"?", "?", "?", "?", "?", "?"};
+	   String[] testPort = {":80", ":0", ":65635", "", ":-1", ":65636", ":65a", };  
+	   String testURLPart1a = "http://google.com";
+	   String testURLPart1b = "http://www.oregonstate.edu";   
+	   String testURLPart2a = "/test1/?action=view";
+	   boolean[] portValidity = {true, true, true, true, false, false, false}; 
 	   int i;
 	   
 	   for(i = 0; i < testPort.length; i++)
 	   {
 		   System.out.println("Test Port: " + testPort[i]);
-		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid("3ht://256.256.256.256" + testPort[i] + "/..") + "URL Should be: " + "?");
-		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid("http:/.1.2.3.4" + testPort[i] + "/..//file?action=view"));
-		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid("h3t://255.255.255.255" + testPort[i] + "/$23"));
-		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid("http/aaa" + testPort[i] + "/test1/file?action=view"));
-		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid("http:/.1.2.3.4" + testPort[i]));
-		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid("http://www.google.com" + testPort[i] + "/test1?action=view"));
-		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid("ftp://go.com" + testPort[i] + "/test1/file?action=edit&mode=up"));
-		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid("h3t://www.google.com" + testPort[i] + "?action=view"));
-		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid("ftp://www.google.com" + testPort[i]));
-		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid("h3t://0.0.0.0" + testPort[i] + "/test1?action=edit&mode=up"));
+		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid(testURLPart1a + testPort[i] + testURLPart2a));
+		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid(testURLPart1a + testPort[i]));
+		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid(testURLPart1b + testPort[i] + testURLPart2a));
+		   System.out.println("Port is " + portValidity[i] + ": " + urlVal.isValid(testURLPart1b + testPort[i]));	   
 	   }
 
    }
@@ -125,11 +119,10 @@ public class UrlValidatorTest extends TestCase
    {
 	   //You can use this function to implement your Second Partition testing	   
 	   String testName = "Partitioned - Path";
-	   System.out.println("Test: " + testName);
+	   System.out.println("\nTest: " + testName);
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 	   String[] testPath= {"", "/test1", "/test1/test2", "/...", "/#", "/test1/#"};  
-	   String[] pathValidity = {"Valid", "Valid", "Valid", "Invalid", "Invalid", "Invalid"}; 
-	   //String[] expectedURLValidity = {"?", "?", "?", "?", "?", "?"};
+	   boolean[] pathValidity = {true, true, true, false, false, false}; 
 	   int i;
 	   
 	   for(i = 0; i < testPath.length; i++)
@@ -148,11 +141,10 @@ public class UrlValidatorTest extends TestCase
    {
 	   //You can use this function to implement your Second Partition testing	   
 	   String testName = "Partitioned - Query";
-	   System.out.println("Test: " + testName);
+	   System.out.println("\nTest: " + testName);
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 	   String[] testQuery= {"", "?action=view", "?action=edit&mode=up"};  
-	   String[] queryValidity = {"Valid", "Valid", "Valid"}; 
-	   //String[] expectedURLValidity = {"?", "?", "?", "?", "?", "?"};
+	   boolean[] queryValidity = {true, true, true}; 
 	   int i;
 	   
 	   for(i = 0; i < testQuery.length; i++)
@@ -173,3 +165,4 @@ public class UrlValidatorTest extends TestCase
 	   System.out.println("Test: random change");
 
    }
+}
